40dae5608cc85c556d5afde838500af17d312de6
lioness3 <jcnailtech3@yahoo.com>
2019-12-31 16:19:21 -0800

    display words on homepage

--- app.rb
+++ app.rb
@@ -5,21 +5,21 @@ require 'pry'
 also_reload('lib/**/*.rb')
 
 get('/') do
+  @study_terms = Word.all
   erb(:home)
 end
-post('/home') do
+get('/home') do
+  @study_terms = Word.all
   new_word = params[:new_word]
   new_definition = params[:new_definition]
   word = Word.new(new_word, new_definition)
   word.save()
-  @study_terms = Word.all
-  erb(:display)
+  erb(:home)
 end
-get('/home/display') do
-  @study_terms = Word.all
-  erb(:edit)
+post('/home') do
+  redirect to('/home')
 end
-patch('/home/display') do
+patch('/home') do
   @study_terms = Word.all
   @new_definition = @word.edit(params[:new_definition])
   @new_word = self.new_word
