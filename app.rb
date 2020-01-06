e51815dd10397806af7d46d4090f4e39ea8ce5de
lioness3 <jcnailtech3@yahoo.com>
2020-01-05 16:12:08 -0800

    add post method for edit page

--- app.rb
+++ app.rb
@@ -46,9 +46,26 @@ end
 
 patch('/home/edit') do
   @term = Word.find(@chosen_word)
-  @term.edit(params[:new_definition])
-  @term.edit(params[:alternate_definition])
+  @vocab = Defintion.find(params[:new_definition])
+  @vocab.edit(params[:new_definition])
+  @vocab2 = Definition.find(params[:alternate_definition])
+    @vocab2.edit(params[:alternate_definition])
+
   @study_terms = Word.all()
   @definitions = Definition.all()
+  erb(:edit)
+end
+post('/home/edit') do
+  new_word = params[:new_word]
+  new_definition = params[:new_definition]
+  alternate_definition = params[:alternate_definition]
+  word = Word.new(new_word, new_definition)
+  word.add_definition(params[:alternate_definition])
+  define = Definition.new(new_word, new_definition, alternate_definition)
+  @chosen_word = word.new_word
+  word.save()
+  define.save()
+  @definitions = Definition.all()
+  @study_terms = Word.all()
   erb(:home)
 end
