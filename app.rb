fa91c54bfcede4251e13793018b599d64bb3ed91
lioness3 <jcnailtech3@yahoo.com>
2020-01-12 17:07:41 -0800

    troubleshoot

--- app.rb
+++ app.rb
@@ -40,31 +40,43 @@ get('/words/:id') do
   @definitions = Definition.all()
   erb(:word)
 end
-
+get('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new({:name => params[:new_definition], :word_id => @word.id, :id => nil})
+  definition.save
+  @definitions = Definition.all()
+  @words = Word.all
+  erb(:word)
+end
 post('/words/:id/definitions') do
   @word = Word.find(params[:id].to_i())
   definition = Definition.new({:name => params[:new_definition], :word_id => @word.id, :id => nil})
-
   definition.save
   @definitions = Definition.all()
-    @words = Word.all
+  @words = Word.all
   erb(:word)
 end
 
-
 get('/words/:id/definitions/:definition_id') do
-  @word = Word.find(params[:id].to_i())
+  word = Word.find(params[:id].to_i())
   @definition = Definition.find(params[:definition_id].to_i())
-    @definitions = Definition.all()
-      @words = Word.all
+  @definitions = Definition.all()
+  @words = Word.all
 erb(:edit_definitions)
 end
-
-
-patch('/words/:id/definitions/:definition_id') do
+post('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id].to_i())
   @definition = Definition.find(params[:definition_id].to_i())
-  @definition.update(params[:update_definition], @word.id)
+  @definition.update(update, @word.id)
+  @definitions = Definition.all()
+  @words = Word.all
+erb(:word)
+end
+patch('/words/:id/definitions/:definition_id') do
+  update = params[:update_definition]
+  @word = Word.find(params[:id].to_i())
+  @definition = Definition.find(params[:definition_id].to_i())
+  @definition.update(update, @word.id)
   @words = Word.all()
   @definitions = Definition.all()
   erb(:word)
@@ -74,6 +86,7 @@ delete('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id])
   @definition = Definition.find(params[:definition_id].to_i())
   @definition.delete
-    @words = Word.all
-  erb(:edit)
+    @definitions = Definition.all()
+  @words = Word.all
+  erb(:word)
 end
