8f3a8bda083460d14d3278becadc0227ef5f0949
lioness3 <jcnailtech3@yahoo.com>
2020-01-12 21:05:59 -0800

    pass integration

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,9 +3,19 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+describe('word path ', {:type => :feature}) do
+  it('creates a word and adds it to the page') do
+    visit('/')
+     if page.has_field?('.edit_definition')
+      fill_in('name', :with => 'SHMO')
+      click_on('ADD')
+      expect(page).to have_content('SHMO')
+    end
+  end
+end
 describe('definition path ', {:type => :feature}) do
   it('edits definition on edit page and displays it on home page') do
-    visit('/home/edit/:id')
+    visit('/words/edit/:id')
      if page.has_field?('.edit_definition')
     fill_in('edited_definition', :with => 'SHMO')
     click_on('Save Changes')
